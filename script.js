01676165288efb3dec74fe46f4d896fb09f5c073
sooan-5206 <annguyen050206@gmail.com>
2026-02-18 10:26:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+const toggle = document.getElementById("toggle-theme");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
