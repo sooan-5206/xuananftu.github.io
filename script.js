901e47467d61e7d5586219c9cab6127219262eb4
sooan-5206 <annguyen050206@gmail.com>
2026-02-18 10:44:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const sections = document.querySelectorAll('.hidden');
-
-window.addEventListener('scroll', () => {
-  sections.forEach(section => {
-    if (section.getBoundingClientRect().top < window.innerHeight - 100) {
-      section.classList.add('show');
-    }
-  });
-});
-
 const toggle = document.getElementById("toggle-theme");
 
 toggle.addEventListener("click", () => {
@@ -24,6 +14,3 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-
-
-
